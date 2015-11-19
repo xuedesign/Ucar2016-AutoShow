b9022b2ea83fc1ca14073046eaeab0c4a9ea8db4
xuedesign <elmo@xuedesign.tw>
2015-11-20 00:57:59 +0800

    品牌館總覽文章頁完成 #20151120

--- script/web_set.js
+++ script/web_set.js
@@ -15,3 +15,12 @@ window.onload = externalLinks;
 $(function() {
           $("img").lazyload({placeholder : "../images/grey.gif",effect: "fadeIn"});
       });
+
+//FB分享
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));	  
